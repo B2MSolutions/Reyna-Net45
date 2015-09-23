5ad1faa670244e326d4d8554290c076c70d0f129
ejarlewski <dan@wiseowlsoftware.co.uk>
2015-09-23 11:22:20 +0100

    Created initial project files for Reyna-Net45

---
+++ src/reyna/Reyna.Facts/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Reyna;
+
+namespace Reyna.Facts
+{
+    
+    public class Class1
+    {
+        [Fact]
+        public void test1()
+        {
+            Reyna.Class1 t = new Reyna.Class1();
+            Assert.True(t.TestThis());
+        }
+    }
+}
