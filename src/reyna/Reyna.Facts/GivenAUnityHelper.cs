25bf4b8454e922c58fdc5bb63ae278da12f25757
ejarlewski <dan@wiseowlsoftware.co.uk>
2015-10-12 15:41:48 +0100

    Fixed failing unit test

--- src/reyna/Reyna.Facts/GivenAUnityHelper.cs
+++ src/reyna/Reyna.Facts/GivenAUnityHelper.cs
@@ -41,8 +41,9 @@ namespace Reyna.Facts
             Assert.NotNull(container.Registrations.FirstOrDefault(r => r.RegisteredType == typeof(IConnectionInfo) && r.MappedToType == typeof(ConnectionInfo)));
             Assert.NotNull(container.Registrations.FirstOrDefault(r => r.RegisteredType == typeof(IBlackoutTime) && r.MappedToType == typeof(BlackoutTime)));
             Assert.NotNull(container.Registrations.FirstOrDefault(r => r.RegisteredType == typeof(IServicePoint) && r.MappedToType == typeof(ServicePointFacade)));
+            Assert.NotNull(container.Registrations.FirstOrDefault(r => r.RegisteredType == typeof(IWebRequest) && r.MappedToType == typeof(ReynaWebRequest)));
 
-            Assert.Equal(17, container.Registrations.Count()); // Always 1 ahead due to the default lifetime manager registration
+            Assert.Equal(18, container.Registrations.Count()); // Always 1 ahead due to the default lifetime manager registration
         }
     }
 }
